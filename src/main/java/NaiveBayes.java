a44a4d49ce90c028ab3dc02c1381f231bd0aeea9
Mert Eroğlu <merteroglu16@gmail.com>
2018-04-30 17:14:27 +0300

    Zemberek denemeleri yapıldı

---
+++ src/main/java/NaiveBayes.java
@@ -0,0 +1,23 @@
+import zemberek.morphology.analysis.tr.TurkishMorphology;
+import zemberek.normalization.TurkishSpellChecker;
+
+import java.io.IOException;
+
+public class NaiveBayes {
+    public static void main(String[] args) {
+        try {
+            TurkishMorphology morphology = TurkishMorphology.createWithDefaults();
+            TurkishSpellChecker spellChecker = new TurkishSpellChecker(morphology);
+
+            System.out.println("Give suggestions.");
+            String[] toSuggest = {"Kraamanda", "okumuştk", "yapbileceksen", "oukyamıyorum"};
+            for (String s : toSuggest) {
+                System.out.println(s + " -> " + spellChecker.suggestForWord(s));
+            }
+            
+        }catch (IOException e){
+
+        }
+
+    }
+}
