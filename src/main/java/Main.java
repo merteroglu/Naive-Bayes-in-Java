5bc9b6e56088d04acead44acf73866cee205b803
Mert Eroğlu <merteroglu16@gmail.com>
2018-05-07 22:00:06 +0300

    Ngram ayırma fonksiyonu eklendi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,21 @@
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
 public class Main {
 
-
     public static void main(String[] args) {
+        NGrams nGrams = new NGrams();
+        String[] arry1 = new String[]{"mert","mert","eroglu"};
+        String[] arry2 = new String[]{"test","deneme","kou","mert"};
+        String[] arry3 = new String[]{"telefon","bilgisayar","java","dene","oglu"};
+        List<String[]> list = new ArrayList<String[]>();
+        list.add(arry1);
+        list.add(arry2);
+        list.add(arry3);
+        Hashtable<String,Integer> tableNGram = nGrams.getNGram(2,list);
+
 
     }
+
 }
--- src/main/java/NGrams.java
+++ src/main/java/NGrams.java
@@ -5,9 +5,19 @@ public class NGrams {
 
     public Hashtable<String,Integer> getNGram(int size , List<String[]> wordList){
         Hashtable<String,Integer> tableNGram = new Hashtable<String, Integer>();
-        //TODO Size değişkeni 2 ngram ya da 3ngram belirler. Wordlistte Listenin her indexi bir dosyayı temsil eder.
-        //TODO Listedeki her indexte o dosyadaki kelimeler bulunur
 
+        for(String[] arr : wordList){
+            for(String str : arr){
+                for (int i = 0; i < str.length() - size + 1; i++) {
+                    String subStr = str.substring(i,i+size);
+                    try{
+                        tableNGram.put(subStr,tableNGram.get(subStr).intValue() + 1);
+                    }catch (Exception e){
+                        tableNGram.put(subStr,1);
+                    }
+                }
+            }
+        }
 
         return tableNGram;
     }
